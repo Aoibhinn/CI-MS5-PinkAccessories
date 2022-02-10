c0d97cd1240ed0baadbd51d190e2807846484c19
Aoibhinn <97aoibhinn@gmail.com>
2022-02-10 21:54:52 +0000

    Add comments and format product views

--- products/views.py
+++ products/views.py
@@ -1,16 +1,27 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-
+# Internal:
 from .models import Product, Category
 from .forms import ProductForm
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 @login_required
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """
+    A view to show all products, including sorting and search queries
+    Args:
+        request (object): HTTP request object.
+    Returns:
+        Render of products page with context
+    """
 
     products = Product.objects.all()
     query = None
@@ -26,25 +37,21 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey == 'category__name'
-
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -61,8 +68,14 @@ def all_products(request):
 
 @login_required
 def product_detail(request, product_id):
-    """ A view to show all individual product details """
-
+    """
+    A view to show individual product details
+    Args:
+        request (object): HTTP request object.
+        product_id: Product id
+    Returns:
+        Render of product detail page with context
+    """
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
@@ -74,11 +87,16 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-    """ Add a product to the store """
+    """
+    A view to add a product
+    Args:
+        request (object): HTTP request object.
+    Returns:
+        Render of add product page with context
+    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -89,7 +107,7 @@ def add_product(request):
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -100,7 +118,14 @@ def add_product(request):
 
 @login_required
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+    """
+    A view to add a product
+    Args:
+        request (object): HTTP request object.
+        product_id: Product id
+    Returns:
+        Render of edit product page with context
+    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -128,8 +153,14 @@ def edit_product(request, product_id):
 
 
 def delete_product(request, product_id):
-    """ Delete a product from the store """
-
+    """
+    A view to delete a product
+    Args:
+        request (object): HTTP request object.
+        product_id: Product id
+    Returns:
+        Render of delete product page with context
+    """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
