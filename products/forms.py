c499b99ea58ffd69d162edcfd9277c95a7cd46a4
Aoibhinn <97aoibhinn@gmail.com>
2022-02-10 22:11:15 +0000

    Add comments and format product forms file

--- products/forms.py
+++ products/forms.py
@@ -1,17 +1,38 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django import forms
+
+# Internal:
 from .widgets import CustomClearableFileInput
 from .models import Product, Category
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
 
 class ProductForm(forms.ModelForm):
+    """
+    A class for product forms
+    """
 
     class Meta:
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False,
+                            widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
+        """
+        Field updates in the form
+        Args:
+            self (object): Self object
+            *args: *args
+            **kwargs: **kwargs
+        Returns:
+            N/A
+        """
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
@@ -19,4 +40,3 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
-
