b08e13ffbb47958ba8396383310d025e4b171c0f
Aoibhinn <97aoibhinn@gmail.com>
2022-02-10 22:24:52 +0000

    Add comments and format product admin file

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,17 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
+
+# Internal:
 from .models import Product, Category
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Product model.
+    """
     list_display = (
         'sku',
         'name',
@@ -13,11 +23,16 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Category model.
+    """
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
