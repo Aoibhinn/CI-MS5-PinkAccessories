cfae089c3650fe9ac284d61a330a6820c4d40a5c
Aoibhinn <97aoibhinn@gmail.com>
2022-02-12 18:25:18 +0000

    change debug to false and remove secret key

--- pink_accessories/settings.py
+++ pink_accessories/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '!z6-swf5)g5iw960^25ffdjj(ja*8sm44hi*j7n=5w2a-3_^4o')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['ci-ms5-pinkaccessories.herokuapp.com', 'localhost']
 
