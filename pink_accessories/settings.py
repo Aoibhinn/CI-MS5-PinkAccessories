40d0d88246b025ac60b27bfa58e4cadf7012d646
Aoibhinn <97aoibhinn@gmail.com>
2022-02-12 17:49:33 +0000

    bug fix: indentation on settings.py databases

--- pink_accessories/settings.py
+++ pink_accessories/settings.py
@@ -130,10 +130,10 @@ if 'DATABASE_URL' in os.environ:
 else:
     DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
