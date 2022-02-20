414ffb6f219f4771f0608fd2ef9ee4383bb4c4e4
Aoibhinn <97aoibhinn@gmail.com>
2022-02-20 18:35:29 +0000

    Falke 8 reformating checkout signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,9 +1,24 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.apps import AppConfig
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """
+    A class for the checkout configuration
+    """
     name = 'checkout'
 
     def ready(self):
+        """
+        import checkout signals library
+        Args:
+            N/A
+        Returns:
+            N/A
+        """
         import checkout.signals
