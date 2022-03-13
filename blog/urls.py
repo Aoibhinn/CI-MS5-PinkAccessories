821d3804a431e6b9be6de7b84e9d4c024db99ff6
Aoibhinn <97aoibhinn@gmail.com>
2022-03-13 20:57:59 +0000

    Bug fixes: remove links to pages on manage blog items, fix link to add blog items in view

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ from . import views
 urlpatterns = [
     path('', views.blog_items, name='blog_items'),
     path('add_blog_item/', views.add_blog_item, name='add_blog_item'),
+    path('edit_blog_item/', views.edit_blog_item, name='edit_blog_item'),
     path('manage_blog_items/', views.manage_blog_items, name='manage_blog_items'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from .forms import BlogForm
@@ -14,25 +14,27 @@ def blog_items(request):
     and blog_items_drafts
     """
 
-    blog_items_published = Blog.objects.filter(status=1).order_by('-create_date')
-    blog_items_drafts = Blog.objects.filter(status=0).order_by('-create_date')
+    blog_items_published = \
+        Blog.objects.filter(status=1).order_by('-create_date')
+    blog_items_drafts = \
+        Blog.objects.filter(status=0).order_by('-create_date')
 
     context = {
         'blog_items_published': blog_items_published,
         'blog_items_drafts': blog_items_drafts,
     }
 
-    return render(request, 'blog/blog.html', context)
+    return render(request, 'blog.html', context)
 
 
 def manage_blog_items(request):
     """
     A view to manage all blog items
     """
-    blog_items = Blog.objects.order_by('-create_date')
+    all_news_items = Blog.objects.order_by('-create_date')
 
     context = {
-        'blog_items': blog_items,
+        'blog_items': all_news_items,
     }
 
     return render(request, 'blog/manage_blog_items.html', context)
@@ -66,4 +68,40 @@ def add_blog_item(request):
     context = {
         'post_form': blog_form,
     }
+    return render(request, template, context)
+
+
+@login_required
+def edit_blog_item(request, blog_item_id):
+    """
+    A view to editing blog items
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    blog_item = get_object_or_404(Blog, pk=blog_item_id)
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST, request.FILES, instance=blog_item)
+        if blog_form.is_valid():
+            blog_form.save()
+            messages.success(request, f'{blog_item.title} '
+                                      f'was successfully updated')
+            """return redirect(reverse('edit_blog item', 
+            args=[news_item.id]))-->"""
+            return redirect('manage_blog_items')
+        else:
+            messages.error(
+                request, f'Error, {blog_item.title} \
+                was not successfully updated')
+    else:
+        blog_form = BlogForm(instance=blog_item)
+        messages.info(request, f'You are currently editing {blog_item.title}')
+
+    template = 'edit_blog_item.html'
+    context = {
+        'blog_form': blog_form,
+        'blog_item': blog_item,
+    }
+
     return render(request, template, context)
